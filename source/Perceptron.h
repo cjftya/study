79e1bcc666564b507304f0010db1bc92c151152c
cjftya <cjftya@gmail.com>
2018-03-18 21:25:24 +0900

    modify file

--- source/Perceptron.h
+++ source/Perceptron.h
@@ -13,29 +13,6 @@ public:
         
     }
     
-    Perceptron(int n, int c) {
-        for(int i=0;i<n;i++) {
-            weightList.push_back(randomValue(-3, 3));
-        }
-        this->c = c;
-    }
-    
-    Vector2d feedForward(Vector2d forces[]) {
-        Vector2d sum;
-        for(int i=0;i<weightList.size();i++) {
-            forces[i].mul(weightList[i]);
-            sum.add(forces[i]);
-        }
-        return sum;
-    }
-    
-    void train(Vector2d forces[], Vector2d error) {
-        for(int i=0;i<weightList.size();i++) {
-            weightList[i] += c * error.x * forces[i].x;
-            weightList[i] += c * error.y * forces[i].y;
-        }
-    }
-    
 };
 
 #endif
